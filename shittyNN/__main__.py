44998c290676df759f9d8a3362decf57acc97711
WizzyGeek <51919967+WizzyGeek@users.noreply.github.com>
2022-03-23 18:34:35 +0530

    [🚲] Training wheels!!!

--- shittyNN/__main__.py
+++ shittyNN/__main__.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from shittyNN.perceptron.training import gradient
+
 class Perceptron_NameSpace_Exp1:
     from . import Perceptron
     fn = "p_wts.npy"
@@ -17,7 +19,7 @@ class Perceptron_NameSpace_Exp1:
 
     @classmethod
     def initialise_wts(cls):
-        wts = np.ones(16)
+        wts = np.zeros(16)
         np.save(cls.fn, wts)
         wts = np.load(cls.fn, allow_pickle=True)
         print(wts)
@@ -32,16 +34,45 @@ class Perceptron_NameSpace_Exp1:
     @classmethod
     def train(cls): # well a perceptron obviously cant diff between a 4x4 1s
         # and horizontal rows so we will limit our selves to detecting first two rows
-        # [
+        # 1/4 *  [
             # 1, 1, 1, 1
             # 1, 1, 1, 1
             # -1, -1, -1, -1
             # -1, -1, -1, -1
         #] These should be our weights for this trivial nueral network equivalent of hello world
-        ...
+        p = cls.Perceptron(np.load(cls.fn, allow_pickle=True))
+        I = cls.Identify
+        inputs = np.array((I.l1, I.l2, I.l3)).reshape((3, 16))
+        print(inputs)
+        targets = [1, 1, 0]
+        weighted_sums = []
+
+        from .perceptron.training import reduction_gradient_vector
+
+        itr = 0
+        while True:
+            for i in (I.l1, I.l2, I.l3):
+                weighted_sums.append(p.compute(i))
+
+            delta = reduction_gradient_vector(targets, weighted_sums, inputs)
+            print(delta)
+            print(p.wt + delta)
+            print("=" * 4, itr)
+
+            p.wt += delta
+            weighted_sums = []
+            itr += 1
+            try:
+                input()
+            except KeyboardInterrupt:
+                break
+
+    # Reduction method worked!!
+    # cons: Whole lot of iterations on the same set, took around 10 iterations
+    # pros: Extremely accurate result
 
 e1 = Perceptron_NameSpace_Exp1
 
 if __name__ == "__main__":
     e1.initialise_wts()
-    e1.run()
+    e1.train()
--- shittyNN/perceptron/training/reduction.py
+++ shittyNN/perceptron/training/reduction.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def reduction_gradient_vector(targets, weighted_sums, inputs):
+def reduction_gradient_vector(targets, weighted_sums, inputs): # Takes
     n = len(targets)
     inputs = np.array(inputs).reshape((n, np.array(inputs[0]).size))
     return sum((i / sum(i)) * (t * max(0, t - a) + (t - 1) * max(0, a)) for i, t, a in zip(inputs, targets, weighted_sums))
